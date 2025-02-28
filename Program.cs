8e0a2e3749d97ff38f753d1de4d2e9cf54cba415
Denis Pi. <zdeniz@mail.ru>
2025-02-28 09:49:09 +0300

    001 Инициализация и тест записи в файл

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string filePath = "example.txt";
+
+// Используем StreamWriter для записи в файл
+using (StreamWriter writer = new StreamWriter(filePath))
+{
+    writer.WriteLine("Первая строка");
+    writer.WriteLine("Вторая строка");
+    writer.WriteLine("Третья строка2");
+}
+
+Console.WriteLine("Файл успешно создан и данные записаны.");
