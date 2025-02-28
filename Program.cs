1ada954413d207c221037008ac49718f94088500
Denis Pi. <zdeniz@mail.ru>
2025-02-28 10:05:03 +0300

    002 Генерация случайного числа и выбор из списка (общий случай)

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создаем список строк
+List<string> specialization = new List<string>();
 
-string filePath = "example.txt";
+// Добавляем элементы
+specialization.Add("Fighter");
+specialization.Add("Thief");
+specialization.Add("Mage");
+
+
+// Создаем объект Random
+Random random = new Random();
+// Генерируем случайное число от 0 до 2 (включительно)
+int randomNumber = random.Next(0, 3);
+// Выводим результат
+Console.WriteLine(specialization[randomNumber]);
+
+//string filePath = "example.txt";
 
 // Используем StreamWriter для записи в файл
-using (StreamWriter writer = new StreamWriter(filePath))
+/* using (StreamWriter writer = new StreamWriter(filePath))
 {
     writer.WriteLine("Первая строка");
     writer.WriteLine("Вторая строка");
     writer.WriteLine("Третья строка2");
 }
 
-Console.WriteLine("Файл успешно создан и данные записаны.");
+Console.WriteLine("Файл успешно создан и данные записаны."); */
